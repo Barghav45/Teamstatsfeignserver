89074c3ac1a0c5470be898bf4758dc240f011705
Barghav@45 <barghavkrishna45@gmail.com>
2022-06-23 10:16:40 +0530

    1st commit

---
+++ src/test/java/TeamManagement/TeamManagement/TeamManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package TeamManagement.TeamManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import TeamStatistics_FeignServer.TeamStatistics_FeignServer.TeamStatisticsApplication;
+
+@SpringBootTest(classes=TeamStatisticsApplication.class )
+class TeamManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
